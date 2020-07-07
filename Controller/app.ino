684f674ac739a1fd44ba1b904357eab6e75178c1
Alexandra de Wit <alexa.dewit@gmail.com>
2020-07-07 19:24:45 -0400

    Record remote

--- Controller/app.ino
+++ Controller/app.ino
@@ -9,33 +9,37 @@
 #define PERIOD_MS 10
 #define MOTION_PIN 2
 #define IR_PIN 8
-#define REMOTE_PWR 0xFD00FF
-#define REMOTE_VOL_UP 0xFD807F
-#define REMOTE_FUNC_STOP 0xFD40BF
-#define REMOTE_PREV 0xFD20DF
-#define REMOTE_PAUSE_PLAY 0xFDA05F
-#define REMOTE_NEXT 0xFD609F
-#define REMOTE_DOWN 0xFD10EF
-#define REMOTE_VOL_DOWN 0xFD906F
-#define REMOTE_UP 0xFD50AF
-#define REMOTE_0 0xFD30CF
-#define REMOTE_EQ 0xFDB04F
-#define REMOTE_ST_REPT 0xFD708F
-#define REMOTE_1 0xFD08F7
-#define REMOTE_2 0xFD8877
-#define REMOTE_3 0xFD48B7
-#define REMOTE_4 0xFD28D7
-#define REMOTE_5 0xFDA857
-#define REMOTE_6 0xFD6897
-#define REMOTE_7 0xFD18E7
-#define REMOTE_8 0xFD9867
-#define REMOTE_9 0xFD58A7
+#define REMOTE_PWR 0xFFA25D
+#define REMOTE_VOL_UP 0xFF629D
+#define REMOTE_FUNC_STOP 0xFFE21D
+#define REMOTE_PREV 0xFF22DD
+#define REMOTE_PAUSE_PLAY 0xFF02FD
+#define REMOTE_NEXT 0xFFC23D
+#define REMOTE_DOWN 0xFFE01F
+#define REMOTE_VOL_DOWN 0xFFA857
+#define REMOTE_UP 0xFF906F
+#define REMOTE_0 0xFF6897
+#define REMOTE_EQ 0xFF9867
+#define REMOTE_ST_REPT 0xFFB04F
+#define REMOTE_1 0xFF30CF
+#define REMOTE_2 0xFF18E7
+#define REMOTE_3 0xFF7A85
+#define REMOTE_4 0xFF10EF
+#define REMOTE_5 0xFF38C7
+#define REMOTE_6 0xFF5AA5
+#define REMOTE_7 0xFF42BD
+#define REMOTE_8 0xFF4AB5
+#define REMOTE_9 0xFF52AD
+#define REMOTE_REPEAT 0xFFFFFFFF
 #define clamp(X, Y, N) (N > Y ? Y : N < X ? X : N)
 
 enum Command : uint8_t
 {
   TogglePower
 };
+IRrecv irrecv(MOTION_PIN);
+decode_results results;
+unsigned long time_now = 0;
 
 void setup()
 {
@@ -48,6 +52,12 @@ void loop()
 {
   if (millis() - time_now > PERIOD_MS)
   {
+    time_now = millis();
+    if (irrecv.decode(&results))
+    {
+      Serial.println(results.value, HEX);
+      irrecv.resume();
+    }
     time_now = millis();
   }
 }
